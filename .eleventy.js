e5cc242f0857687199ddcd90e1b0a19789f6328e
Jason Garber <jason@sixtwothree.org>
2020-12-29 17:12:56 -0500

    Refactor metacrap using webhint.io recommendations

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,12 @@ module.exports = eleventy => {
   eleventy.setBrowserSyncConfig(require('./bs-config.js'));
 
   eleventy.addPassthroughCopy('./src/assets');
+
+  eleventy.addPassthroughCopy('./src/apple-touch-icon.png');
+  eleventy.addPassthroughCopy('./src/favicon.ico');
+  eleventy.addPassthroughCopy('./src/icon-256x256.png');
+  eleventy.addPassthroughCopy('./src/icon-384x384.png');
+  eleventy.addPassthroughCopy('./src/icon-512x512.png');
   eleventy.addPassthroughCopy('./src/manifest.webmanifest');
   eleventy.addPassthroughCopy('./src/robots.txt');
 
