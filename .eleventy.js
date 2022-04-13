c58b54df045a449fec6db526ff87c9c458b8608f
Jason Garber <jason@sixtwothree.org>
2022-04-13 14:42:47 -0400

    Add SVG icon for BIMI

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = eleventyConfig => {
 
   eleventyConfig.addPassthroughCopy('./src/apple-touch-icon.png');
   eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('./src/icon.svg');
   eleventyConfig.addPassthroughCopy('./src/icon-256x256.png');
   eleventyConfig.addPassthroughCopy('./src/icon-384x384.png');
   eleventyConfig.addPassthroughCopy('./src/icon-512x512.png');
