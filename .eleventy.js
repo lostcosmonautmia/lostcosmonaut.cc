54c7432087e0a9b32685a5275656ce8339ae56a4
Jason Garber <jason@sixtwothree.org>
2022-04-14 13:50:35 -0400

    Reorder passthrough files

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,10 @@ module.exports = eleventyConfig => {
 
   eleventyConfig.addPassthroughCopy('./src/apple-touch-icon.png');
   eleventyConfig.addPassthroughCopy('./src/favicon.ico');
-  eleventyConfig.addPassthroughCopy('./src/icon.svg');
   eleventyConfig.addPassthroughCopy('./src/icon-256x256.png');
   eleventyConfig.addPassthroughCopy('./src/icon-384x384.png');
   eleventyConfig.addPassthroughCopy('./src/icon-512x512.png');
+  eleventyConfig.addPassthroughCopy('./src/icon.svg');
   eleventyConfig.addPassthroughCopy('./src/robots.txt');
 
   return {
