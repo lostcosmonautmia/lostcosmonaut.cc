4fa4ea34d497cf26bf90c98bef39c344c7955ed8
Jason Garber <jason@sixtwothree.org>
2023-01-06 10:25:55 -0500

    Simplify passthrough copy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,7 @@
 module.exports = eleventyConfig => {
-  eleventyConfig.addPassthroughCopy('./src/assets');
-
-  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon.png');
-  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
-  eleventyConfig.addPassthroughCopy('./src/icon-256x256.png');
-  eleventyConfig.addPassthroughCopy('./src/icon-384x384.png');
-  eleventyConfig.addPassthroughCopy('./src/icon-512x512.png');
-  eleventyConfig.addPassthroughCopy('./src/icon.svg');
-  eleventyConfig.addPassthroughCopy('./src/robots.txt');
+  eleventyConfig
+    .addPassthroughCopy('./src/assets')
+    .addPassthroughCopy('./src/*.{ico,png,svg,txt}');
 
   return {
     dir: {
