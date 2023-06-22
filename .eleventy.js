83a82f4d3807279df4e0d68c7c8522c786b41fc2
Jason Garber <jason@sixtwothree.org>
2023-06-21 22:10:52 -0400

    Refactor data to use webmanifest

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
-module.exports = eleventyConfig => {
+module.exports = function(eleventyConfig) {
+  // Global Data
+  eleventyConfig.addGlobalData('app', require('./src/manifest.webmanifest.json'));
+
+  // Passthrough File Copy
   eleventyConfig
     .addPassthroughCopy('./src/*.{ico,png,svg,txt}')
-    .addPassthroughCopy('./src/assets');
+    .addPassthroughCopy('./src/assets')
+    .addPassthroughCopy({
+      './src/manifest.webmanifest.json': 'manifest.webmanifest'
+    });
 
   return {
     dir: {
