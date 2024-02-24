53fb07a2d16ab90d15e788546133322860983246
Jason Garber <jason@sixtwothree.org>
2024-02-23 23:33:13 -0500

    Address linting issues

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,19 +1,19 @@
 module.exports = function(eleventyConfig) {
   // Global Data
-  eleventyConfig.addGlobalData('app', require('./src/manifest.webmanifest.json'));
+  eleventyConfig.addGlobalData("app", require("./src/manifest.webmanifest.json"));
 
   // Passthrough File Copy
   eleventyConfig
-    .addPassthroughCopy('./src/_headers')
-    .addPassthroughCopy('./src/*.{ico,png,svg,txt}')
-    .addPassthroughCopy('./src/assets')
+    .addPassthroughCopy("./src/_headers")
+    .addPassthroughCopy("./src/*.{ico,png,svg,txt}")
+    .addPassthroughCopy("./src/assets")
     .addPassthroughCopy({
-      './src/manifest.webmanifest.json': 'manifest.webmanifest'
+      "./src/manifest.webmanifest.json": "manifest.webmanifest",
     });
 
   return {
     dir: {
-      input: './src'
-    }
+      input: "./src",
+    },
   };
 };
